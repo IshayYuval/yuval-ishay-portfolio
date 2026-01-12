68a7142f21c4bbfdfddd82e7e0d1cebb3a9e827d
Yuval Ishay <yuvali@my.hit.ac.il>
2026-01-12 17:48:14 +0200

    added a 301 redirect for seo reasons

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/welcome',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
